6d5678d2d4338fc633d645af031b7266f2185a9a
saivivek mac <saivivek116@gmail.com>
2023-10-08 03:12:53 -0400

    changes

--- script.js
+++ script.js
@@ -16,3 +16,33 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 });
+
+const breadButton = document.querySelector(".bread");
+const nav = document.querySelector("nav");
+
+breadButton.addEventListener("click", function () {
+  nav.classList.toggle("collapsed");
+});
+
+const navItems = document.querySelector(".nav-items");
+let activeLink = null;
+
+navItems.addEventListener("click", function (event) {
+  if (event.target.tagName === "A") {
+    event.preventDefault();
+
+    const clickedLink = event.target;
+
+    if (activeLink) {
+      activeLink.classList.remove("active");
+    }
+
+    clickedLink.classList.add("active");
+    activeLink = clickedLink;
+  }
+});
+
+const container = document.querySelector(".container");
+container.addEventListener("click", () => {
+  container.classList.toggle("cancel");
+});
